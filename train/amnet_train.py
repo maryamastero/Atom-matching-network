29648766983c160a764211518cd015a87a6bde3c
Maryam Astero <60658276+maryamastero@users.noreply.github.com>
2024-01-11 11:37:32 +0200

    Update amnet_train.py

--- train/amnet_train.py
+++ train/amnet_train.py
@@ -3,7 +3,7 @@ import torch
 from torch_geometric.data import DataLoader
 from gin import GIN
 import pandas as pd
-from amnet import AMNet
+from fmnet import FMNet
 import rdkit.Chem as Chem
 from molgraphdataset import *
 import pickle
@@ -19,43 +19,41 @@ n_edge_features = len(get_bond_features(silly_mol.GetBondBetweenAtoms(0,1)))
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_wl_iterations', type = int, default = 3)
 parser.add_argument('--node_features_dim', type = int, default = n_node_features)
-parser.add_argument('--edge_feature_dim', type = int, default = None)#n_edge_features
-parser.add_argument('--santitize', type = int, default = False)
-parser.add_argument('--embedding_dim', type = int, default=128)
-parser.add_argument('--second_embedding_dim', type=int, default=64)
+parser.add_argument('--edge_feature_dim', type = int, default = n_edge_features)
+parser.add_argument('--santitize', type = bool, default = False)
+parser.add_argument('--embedding_dim', type = int, default=512)
 parser.add_argument('--num_layers', type = int, default = 3)
 parser.add_argument('--lr', type=float, default = 0.0001)
-parser.add_argument('--num_concensus_iter', type=int, default=200)
-parser.add_argument('--n_epochs', type = int, default = 5)
+parser.add_argument('--n_epochs', type = int, default = 200)
 parser.add_argument('--batch_size', type = int, default = 1)
 
 args = parser.parse_args()
 
+train_data = pd.read_csv('MoleculeDataset/train.csv', names= ['reactions', 'edits'])
+valid_data =pd.read_csv('MoleculeDataset/valid.csv', names= ['reactions', 'edits'])
 
-train_data = pd.read_csv('MoleculeDataset/vcna_train.csv', names= ['reactions', 'edits'])
-valid_data =pd.read_csv('MoleculeDataset/vcna_valid.csv', names= ['reactions', 'edits'])
-whole_data = pd.concat([train_data, valid_data])
-train_dataset = MolGraphDataset(whole_data , args.num_wl_iterations, santitize=args.santitize)
-train_loader = DataLoader(train_dataset, args.batch_size, shuffle=True,follow_batch=['x_r', 'x_p'])
+train_dataset = MolGraphDataset(train_data , args.num_wl_iterations, santitize=args.santitize)
+train_loader = DataLoader(train_dataset, args.batch_size, shuffle=False,follow_batch=['x_r', 'x_p'])
 
-validation_dataset = MolGraphDataset(valid_data, args.num_wl_iterations, santitize=args.santitize)
+validation_dataset = MolGraphDataset(train_data, args.num_wl_iterations, santitize=args.santitize)
 validation_loader = DataLoader(validation_dataset, args.batch_size, shuffle=False,follow_batch=['x_r', 'x_p'])
 
+gnn = GIN(args.node_features_dim, args.embedding_dim, num_layers=args.num_layers, cat=True )
+model = FMNet(gnn)
 
-gnn_1 = GIN(args.node_features_dim, args.embedding_dim, num_layers=args.num_layers, cat=False )
-gnn_2 = GIN(args.second_embedding_dim, args.second_embedding_dim, num_layers=args.num_layers, cat=True)
-model = AMNet(gnn_1, gnn_2, num_concensus_iter=args.num_concensus_iter)
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.95, 0.999))
+print(device)
 print(model)
 print(args)
 
-gnn_1 =  gnn_1.to(device)
-gnn_2 =  gnn_2.to(device)
+
+gnn =  gnn.to(device)
 model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.95, 0.999))
 
 best_valid_loss = float('inf')
 patience = 20  # Number of epochs to wait for improvement
 counter = 0  # Counter to keep track of epochs without improvement
+
 
 
 
@@ -66,26 +64,24 @@ def train(train_loader):
     for i, data in enumerate(train_loader):   
         optimizer.zero_grad()
         data = data.to(device)
-        M0_hat, Mt_hat  = model( data.x_r,data.edge_index_r,data.edge_feat_r,
-                                data.x_p, data.edge_index_p,data.edge_feat_p,
-                                data.batch_size)
-
-        M_0 = model.adjust_correspondence_matrix_for_symmetry(M0_hat, data.eq_as[0])
-        M_T = model.adjust_correspondence_matrix_for_symmetry(Mt_hat, data.eq_as[0])
-
-        loss = model.loss(M_0, data.y_r, data.y_p)
-        loss = model.loss(M_T, data.y_r, data.y_p) + loss
+        #print(i)
+        M_hat = model( data.x_r,data.edge_index_r,data.edge_feat_r,
+                            data.x_p, data.edge_index_p,data.edge_feat_p,
+                            data.batch_size) 
+       
+        
+        M = model.symmetrywise_correspondence_matrix(M_hat,data.eq_as[0],data.rp_mapper)
+        loss = model.loss(M, data.y_r,  data.rp_mapper)
 
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
 
-        total_correct += model.acc(M_T , data.y_r, data.y_p, reduction='sum')
+        total_correct += model.acc(M , data.y_r, data.rp_mapper, reduction='sum')
         total_nodes += data.y_r.size(0)
-        
+     
         
     return total_loss/len(train_loader), total_correct /total_nodes
-
 
 def validation_loss(loader):
     model.eval()
@@ -93,18 +89,17 @@ def validation_loss(loader):
     with torch.no_grad():
         for data in loader:
             data = data.to(device)
-            M0_hat, Mt_hat  = model( data.x_r,data.edge_index_r,data.edge_feat_r,
-                        data.x_p, data.edge_index_p,data.edge_feat_p, data.batch_size)
+            
+            M_hat = model( data.x_r,data.edge_index_r,data.edge_feat_r,
+                                data.x_p, data.edge_index_p,data.edge_feat_p,
+                                data.batch_size) 
         
-            M_0 = model.adjust_correspondence_matrix_for_symmetry(M0_hat, data.eq_as[0])
-            M_T = model.adjust_correspondence_matrix_for_symmetry(Mt_hat, data.eq_as[0])
-
-            loss = model.loss(M_0, data.y_r, data.y_p)
-            loss = model.loss(M_T, data.y_r, data.y_p) + loss
-
+        
+            M = model.symmetrywise_correspondence_matrix(M_hat, data.eq_as[0], data.rp_mapper)
+            loss = model.loss(M, data.y_r, data.rp_mapper)
             total_loss += loss.item()
 
-            total_correct += model.acc(M_T, data.y_r, data.y_p, reduction='mean')
+            total_correct += model.acc(M, data.y_r, data.rp_mapper, reduction='sum')
             total_nodes += data.y_r.size(0)
             
             
@@ -119,7 +114,7 @@ all_valid_acc = []
 for epoch in range(1, args.n_epochs+1):
     print(f'Epoch: {epoch:02d}', 5*'*')
     train_loss , train_acc = train(train_loader)
-    #print(train_loss , train_acc)
+    #print(train_loss, train_acc)
     valid_loss , valid_acc  = validation_loss(validation_loader)
 
     all_train_loss.append(train_loss)
@@ -127,7 +122,6 @@ for epoch in range(1, args.n_epochs+1):
 
     all_valid_loss.append(valid_loss)
     all_valid_acc.append(valid_acc)
-   
 
     if valid_loss < best_valid_loss:
         best_valid_loss = valid_loss
@@ -139,11 +133,10 @@ for epoch in range(1, args.n_epochs+1):
         print(f'Early stopping. No improvement in {epoch} epochs.')
         break
     
-path = 'experiment1/12_12'
+path = 'experiment1/05_10_edege_feature_golden_0'
 
 if not os.path.exists(path):
     os.makedirs(path)
-
 
 torch.save(model.state_dict(), f'{path}/model.pth')
 
@@ -160,7 +153,3 @@ with open(f'{path}/losses_valid.txt', 'wb') as file:
 
 with open(f'{path}/acces_valid.txt', 'wb') as file:
        pickle.dump(all_valid_acc, file)
-
-
-
-        
